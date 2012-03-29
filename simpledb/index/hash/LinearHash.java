2deaa42f136b75539b99563ff03c9a8780a6dbf1
aroberts3@gatech.edu <Alfred@.(none)>
2012-03-29 09:34:44 -0400

    reimplemented printindex, added new array for flagging buckets as viable

--- simpledb/index/hash/LinearHash.java
+++ simpledb/index/hash/LinearHash.java
@@ -4,17 +4,21 @@ import simpledb.tx.Transaction;
 import simpledb.record.*;
 import simpledb.query.*;
 import simpledb.index.Index;
+import simpledb.file.*;
 
 import java.util.ArrayList;
 
+
 public class LinearHash implements Index{
   public int numBuckets = 1;
+  public int startingBuckets = 1;
   private int overflowCount = 0;
   private int maxOverflow = 1;
   private int blockSize = 5; //max records per block
   private int splitPointer = 0; // next bucket to be split
   private boolean redistributing = false; //additional expands will not be performed mid-expand
-  private ArrayList<Integer> records = new ArrayList<Integer>(); //number of records in each bucket including overflow
+  private int[] records = new int[100]; //number of records in each bucket including overflow
+  private int[] buckets = new int[100]; //keeps track of which locations represent viable buckets
   private int level = 0;
   private String idxname;
   private Schema sch;
@@ -26,6 +30,7 @@ public class LinearHash implements Index{
     this.idxname = idxname;
     this.sch = sch;
     this.tx = tx;
+    initializeArrays();
   }
 
   public void beforeFirst(Constant searchkey){
@@ -61,21 +66,14 @@ public class LinearHash implements Index{
 
   public void insert(Constant val, RID rid){
 	close();
-	if(records.size()==0){
-		growArray(100);
-	}
-	System.out.println("--");
 	int bucket = address(val);
-	int numRecords = records.get(bucket);
+	int numRecords = records[bucket];
     beforeFirst(val); 
     ts.insert();
     ts.setInt("block", rid.blockNumber());
     ts.setInt("id", rid.id());
     ts.setVal("dataval", val);
-    records.set(bucket,numRecords+1);
-    System.out.println(bucket);
-    System.out.println(val);
-    System.out.println(records.get(bucket));
+    records[bucket]=numRecords+1;
     if(((numRecords+1)>(blockSize))&&(!redistributing)){
     	overflowCount++;
     	if(overflowCount==maxOverflow){
@@ -87,13 +85,16 @@ public class LinearHash implements Index{
   // Expands the bucket pointed to by the splitPointer
   public void expand(){
 	close();
-	System.out.println("expand");
 	overflowCount=0;
-	records.set(splitPointer, 0);
-    //printIndex();
+	numBuckets++;
+	records[splitPointer]= 0;
+	System.out.println("\nBefore expand: \n-------------------------");
+    printIndex();
     setSplitBucket();
     ArrayList<RID> rids = new ArrayList<RID>();
     ArrayList<Constant> vals = new ArrayList<Constant>();
+    int nextBucket = (int)Math.pow(2,level) * startingBuckets;
+    buckets[nextBucket]=1;
 
     //clear the bucket; contents are saved to be redistributed after
     while(ts.next()){
@@ -116,10 +117,8 @@ public class LinearHash implements Index{
     	insert(dv,r);
     }
     redistributing = false;
-    
-    System.out.println("end expand");
-    
-    //printIndex();
+    System.out.println("\nAfter expand: \n-------------------------");
+    printIndex();
   }
 
   // Closes the index by closing the current table scan
@@ -130,18 +129,22 @@ public class LinearHash implements Index{
   }
 
   public int address(Constant key){
-    double bucket = (key.hashCode() % (Math.pow(2,level)*numBuckets));
+    double bucket = (key.hashCode() % (Math.pow(2,level)*startingBuckets));
     if (bucket<splitPointer){
-    	bucket = (key.hashCode() % ((int)(Math.pow(2,level+1))*numBuckets));
+    	bucket = (key.hashCode() % ((int)(Math.pow(2,level+1))*startingBuckets));
     }
     return (int)bucket;
   }
   
-  public void growArray(int i){
-	  int l = records.size();
-	  while(l<i+1){
-		  records.add(0);
-		  l++;
+  public void initializeArrays(){
+	  int i;
+	  for(i=0;i<records.length;i++){
+		  records[i]=0;
+		  buckets[i]=0;
+	  }
+	  //flag starting buckets as viable
+	  for(i=0;i<startingBuckets;i++){
+		  buckets[i]=1;
 	  }
   }
  
@@ -154,12 +157,15 @@ public class LinearHash implements Index{
   
   public void printIndex(){
     close();
-    System.out.printf("Level: %d \t Next: %d", level, splitPointer);
-    int len = numBuckets;
+    System.out.printf("\n Level: %d \t Next: %d \n \n", level, splitPointer);
     int i;
     int block;
+    String val;
     boolean overflow = false;
-    for(i=0; i<len; i++){
+    for(i=0; i<buckets.length; i++){
+    	if(buckets[i]==0){
+    		continue;
+    	}
       String tblname = idxname + i;
       TableInfo ti = new TableInfo(tblname, sch);
       ts = new TableScan(ti, tx);
@@ -169,15 +175,16 @@ public class LinearHash implements Index{
         block = ts.currentBlock();
         if(block==0){
           // Not in the overflow blocks
-          System.out.printf("%d \t", ts.getInt("id"));
+        	val = ts.getVal("dataval").toString();
+        	System.out.printf(val.substring(val.length()-3) + "\t");
         }
         else{
           if(!overflow){
-            System.out.println();
-            System.out.printf("Bucket #%d overflow\n", i);
+            System.out.printf("\nBucket #%d overflow\n", i);
             overflow = true;
           }
-          System.out.printf("%d \t", ts.getInt("id"));
+          val = ts.getVal("dataval").toString();
+      	  System.out.printf(val.substring(val.length()-3) + "\t");
         }
       }
       System.out.println("\n"); // white space between buckets
